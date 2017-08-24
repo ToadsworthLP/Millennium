3e89aeacc3807e446793ef0757fd02649727e78c
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-08-24 18:59:25 +0200

    Added trigger system

---
+++ Assets/Scripts/Triggers/ExampleTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleTrigger : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject target;
+
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player") && target != null){
+            target.GetComponent<ITriggerable>().triggerObject("Test");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Triggers/ExampleTriggeredObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleTriggeredObject : MonoBehaviour, ITriggerable {
+
+    public void triggerObject(string args) {
+        print("Trigger activated with arguments "+args+" with target "+ gameObject.name);
+        Destroy(gameObject);
+    }
+
+}
