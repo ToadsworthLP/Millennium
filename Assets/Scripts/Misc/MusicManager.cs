7e3588723f7ea6399a19d1e88c12b47055b28144
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-08-25 18:14:35 +0200

    Added some music

---
+++ Assets/Scripts/Misc/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip musicClip;
+    public float playDelay;
+    public bool useLoops;
+    public float loopStart;
+    public float loopEnd;
+
+    void Awake () {
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+        audioSource.clip = musicClip;
+        audioSource.timeSamples = audioSource.clip.frequency;
+        audioSource.timeSamples = 0;
+
+        StopAllCoroutines();
+        audioSource.PlayDelayed(playDelay);
+    }
+	
+	void Update () {
+        if (useLoops)
+        {
+            if (audioSource.timeSamples / (float)audioSource.clip.frequency >= loopEnd)
+            {
+                audioSource.timeSamples = (int)(loopStart * audioSource.clip.frequency);
+            }
+        }
+    }
+}
