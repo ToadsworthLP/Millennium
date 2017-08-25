dc165f3459b8e2ed530dd73d3a98f18d9d297cfe
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-08-25 19:25:12 +0200

    Basic HUD implementation

---
+++ Assets/Scripts/UI/FancyNumberHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FancyNumberHandler : MonoBehaviour {
+
+    public ImageToNumber first;
+    public ImageToNumber second;
+    public ImageToNumber third;
+
+    public int testValue;
+
+    void Update() {
+        UpdateValue(testValue);
+    }
+
+    //This is so lame, copied from SM64 HD remake thing because I'm lazy
+    public void UpdateValue(int value) {
+        if(third != null){
+            value = Mathf.Clamp(value, 0, 999);
+        }else{
+            value = Mathf.Clamp(value, 0, 99);
+        }
+
+        var stringValue = value.ToString();
+
+        if (stringValue.Length == 1) {
+            first.GetComponent<Image>().enabled = true;
+            second.GetComponent<Image>().enabled = false;
+
+            first.SetValue((int)Char.GetNumericValue(stringValue[0]));
+
+            //This is uglier than everything on Pornhub together
+            if (third != null){third.GetComponent<Image>().enabled = false; }
+
+        } else if (stringValue.Length == 2) {
+            first.GetComponent<Image>().enabled = true;
+            second.GetComponent<Image>().enabled = true;
+
+            first.SetValue((int)Char.GetNumericValue(stringValue[0]));
+            second.SetValue((int)Char.GetNumericValue(stringValue[1]));
+
+            //Uh...again
+            if (third != null){third.GetComponent<Image>().enabled = false; }
+
+        } else if (third != null) {
+            first.GetComponent<Image>().enabled = true;
+            second.GetComponent<Image>().enabled = true;
+            third.GetComponent<Image>().enabled = true;
+
+            first.SetValue((int)Char.GetNumericValue(stringValue[0]));
+            second.SetValue((int)Char.GetNumericValue(stringValue[1]));
+            third.SetValue((int)Char.GetNumericValue(stringValue[2]));
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ImageToNumber.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageToNumber : MonoBehaviour {
+
+    public Sprite[] NumberImages;
+
+    public void Start() {
+        SetValue(0);
+    }
+
+    public void SetValue(int value) {
+        value = Mathf.Clamp(value, 0, NumberImages.Length - 1);
+
+        GetComponent<Image>().sprite = NumberImages[value];
+    }
+
+}
