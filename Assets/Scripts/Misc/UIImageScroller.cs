a8487d2807468befc6c817dd29b708f874202c3d
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-09-02 10:27:08 +0200

    Added background animation for the silver-blue text box (used for save blocks)

---
+++ Assets/Scripts/Misc/UIImageScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIImageScroller : MonoBehaviour
+{
+    public Vector2 uvAnimationRate = new Vector2(1.0f, 1.0f);
+    private Image image;
+
+    void Start() {
+        image = GetComponent<Image>();
+    }
+
+    Vector2 uvOffset = Vector2.zero;
+    void LateUpdate() {
+        uvOffset += (uvAnimationRate * Time.deltaTime);
+        if (image.enabled) {
+            image.materialForRendering.mainTextureOffset = uvOffset;
+        }
+    }
+
+}
