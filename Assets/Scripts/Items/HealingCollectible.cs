3f615734f487531cf5df1036f403f33314f39b00
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-10-11 20:33:43 +0200

    Implement proper healing collectibles like hearts and flowers

---
+++ Assets/Scripts/Items/HealingCollectible.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HealingCollectible : MonoBehaviour {
+
+    public AudioClip collectSound;
+    public Sprite[] blinkSprites;
+    public float blinkAnimationLength;
+    public SpriteRenderer art;
+    public StatModifier modifier;
+
+    private Backpack backpack;
+    private Sprite defaultSprite;
+
+    public void blink() {
+        StartCoroutine(blinkingAnimation());
+    }
+
+    IEnumerator blinkingAnimation(){
+        float frameLength = blinkAnimationLength / blinkSprites.Length;
+        foreach(Sprite s in blinkSprites){
+            art.sprite = s;
+            yield return new WaitForSeconds(frameLength);
+        }
+        art.sprite = defaultSprite;
+    }
+
+    void Awake() {
+        backpack = GameObject.FindGameObjectWithTag("Backpack").GetComponent<Backpack>();
+        defaultSprite = art.sprite;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            switch(modifier.statToModify){
+                case StatType.HP:
+                    backpack.hp += modifier.value;
+                    break;
+
+                case StatType.FP:
+                    backpack.fp += modifier.value;
+                    break;
+
+                case StatType.SP:
+                    backpack.sp += modifier.value;
+                    break;
+            }
+            other.gameObject.GetComponent<PlayerMachine>().audioSource.PlayOneShot(collectSound);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Triggers/ExampleTriggeredObject.cs
+++ Assets/Scripts/Triggers/ExampleTriggeredObject.cs
@@ -3,8 +3,8 @@
 public class ExampleTriggeredObject : MonoBehaviour {
 
     [SerializeField]
-    public void trigger(GameObject sender, string args) {
-        print("Trigger activated with message " + args);
+    public void trigger(GameObject sender, object args) {
+        print("Trigger activated with message " + args.ToString());
         GameObject.FindGameObjectWithTag("Backpack").GetComponent<Backpack>().coins += 10;
         Destroy(gameObject);
     }
