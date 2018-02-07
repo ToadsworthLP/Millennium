bff65cee9ef573095aace01e2b7e26ea34b734ce
ToadsworthLP <fiery.toadsworth@gmail.com>
2018-02-07 22:55:34 +0100

    Added the SoundEffectNode

--- Assets/Engine/Scripts/Misc/MusicManager.cs
+++ Assets/Engine/Scripts/Misc/MusicManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class MusicManager : MonoBehaviour {
 
@@ -9,6 +10,32 @@ public class MusicManager : MonoBehaviour {
     public float loopStart;
     public float loopEnd;
 
+    [HideInInspector]
+    public float defaultMusicVolume;
+
+    private Coroutine musicFadeCoroutine;
+
+    public void FadeMusicVolume(float duration, float targetVolume) {
+        if(musicFadeCoroutine != null){
+            StopCoroutine(musicFadeCoroutine);
+        }
+
+        musicFadeCoroutine = StartCoroutine(FadeMusic(duration, targetVolume));
+    }
+
+    private IEnumerator FadeMusic(float duration, float targetVolume) {
+        float startVolume = audioSource.volume;
+        float progress = 0;
+
+        while(progress < 1){
+            audioSource.volume = Mathf.Lerp(startVolume, targetVolume, progress);
+            progress += Time.deltaTime/duration;
+            yield return new WaitForEndOfFrame();
+        }
+
+        audioSource.volume = targetVolume; //Just for safety
+    }
+
     void Awake () {
         audioSource = gameObject.GetComponent<AudioSource>();
 
@@ -17,6 +44,8 @@ public class MusicManager : MonoBehaviour {
         audioSource.timeSamples = 0;
 
         audioSource.PlayDelayed(playDelay);
+
+        defaultMusicVolume = audioSource.volume;
     }
 	
 	void Update () {
