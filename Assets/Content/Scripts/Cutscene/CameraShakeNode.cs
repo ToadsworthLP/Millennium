e9cb5e4a67a4c588afd6f018f38bd0d14a5ce816
ToadsworthLP <fiery.toadsworth@gmail.com>
2018-02-07 17:02:03 +0100

    Added the CameraShakeNode

---
+++ Assets/Content/Scripts/Cutscene/CameraShakeNode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraShakeNode : BaseCutsceneNode
+{
+    public CameraController camera;
+    public float shakeDuration;
+    public float shakeMagnitude;
+    public bool waitForShakingFinished;
+
+    public override void CallNode() {
+        camera.Shake(shakeMagnitude, 10f, shakeDuration);
+        if (waitForShakingFinished){
+            StartCoroutine(WaitUntilFinishedShaking());
+        }else{
+            CallOutputSlot("Next Node");
+        }
+    }
+
+    public override void DeclareOutputSlots() {
+        SetOutputSlot("Next Node");
+    }
+
+    private IEnumerator WaitUntilFinishedShaking() {
+        yield return new WaitForSeconds(shakeDuration);
+        CallOutputSlot("Next Node");
+    }
+
+}
